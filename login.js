9c41bf6a5a4ed61a90bfd4d8d28b68a34710df36
Wanderson Guimarães <contato@nobreuniformes.com>
2025-04-30 01:07:06 -0300

    Adicionando debug no login

--- login.js
+++ login.js
@@ -10,17 +10,18 @@ if (btn) {
   console.log("✅ Botão encontrado");
 
   btn.addEventListener("click", async () => {
-    console.log("🟢 Botão clicado");
-
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
+    console.log("📨 Tentando logar com:", email, senha);
+
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, senha);
+      console.log("✅ Login realizado com:", userCredential);
       alert("Login realizado com sucesso!");
       window.location.href = "painel.html";
     } catch (error) {
-      console.error("🚨 Erro no login:", error);
+      console.error("🚨 Erro no login:", error.code, error.message);
       document.getElementById("erro").innerText = "Erro: " + error.message;
     }
   });
